c6d7cc02e024928142ee65390d3dd2d12d877e49
AWFJ <awfj837@gmail.com>
2023-08-28 13:44:46 +0300

    add stuff

--- Tetris/Tetraminos/TetraminoL.cs
+++ Tetris/Tetraminos/TetraminoL.cs
@@ -1,90 +1,88 @@
-﻿using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using Microsoft.Xna.Framework;
 
-namespace Tetris
+namespace Tetris;
+
+internal class TetraminoL : Tetramino
 {
-    internal class TetraminoL : Tetramino
+    public TetraminoL(Rectangle background)
     {
-        public TetraminoL(Rectangle background)
-        {
-            Color = Color.Orange;
-            Type = new Random().Next(0, 4);
+        Color = Color.Orange;
+        Type = new Random().Next(0, 4);
 
-            Initialize(Color, background);
+        Initialize(Color, background);
+    }
+
+    public override void Rotate(Queue<Block>[] columns)
+    {
+        Block block = Blocks[0][0];
+        Rectangle rectangle = block.Rectangle;
+
+        switch (Type)
+        {
+            case 1:
+                Type = 2;
+
+                rectangle.X -= Block.Length;
+                rectangle.Y += Block.Length;
+                block.Column -= 1;
+                block.Row += 1;
+                break;
+            case 2:
+                Type = 3;
+
+                rectangle.Y -= Block.Length;
+                block.Row -= 1;
+                break;
+            case 3:
+                Type = 0;
+
+                rectangle.Y += Block.Length;
+                break;
+            default:
+                Type = 1;
+
+                rectangle.X += Block.Length;
+                rectangle.Y -= Block.Length;
+                block.Column += 1;
+                break;
         }
 
-        public override void Rotate(Queue<Block>[] columns)
+        Blocks[0][0].Rectangle = rectangle;
+        Initialize(Color, Blocks[0][0]);
+    }
+
+    protected override void SetProperties()
+    {
+        switch (Type)
         {
-            Block block = Blocks[0][0];
-            Rectangle rectangle = block.Rectangle;
-
-            switch (Type)
-            {
-                case 1:
-                    Type = 2;
-
-                    rectangle.X -= Block.Length;
-                    rectangle.Y += Block.Length;
-                    block.Column -= 1;
-                    block.Row += 1;
-                    break;
-                case 2:
-                    Type = 3;
-
-                    rectangle.Y -= Block.Length;
-                    block.Row -= 1;
-                    break;
-                case 3:
-                    Type = 0;
-
-                    rectangle.Y += Block.Length;
-                    break;
-                default:
-                    Type = 1;
-
-                    rectangle.X += Block.Length;
-                    rectangle.Y -= Block.Length;
-                    block.Column += 1;
-                    break;
-            }
-
-            Blocks[0][0].Rectangle = rectangle;
-            Initialize(Color, Blocks[0][0]);
+            case 1:
+            case 3:
+                Columns = 2;
+                Rows = 3;
+                break;
+            case 2:
+            default:
+                Columns = 3;
+                Rows = 2;
+                break;
         }
 
-        protected override void SetProperties()
+        switch (Type)
         {
-            switch (Type)
-            {
-                case 1:
-                case 3:
-                    Columns = 2;
-                    Rows = 3;
-                    break;
-                case 2:
-                default:
-                    Columns = 3;
-                    Rows = 2;
-                    break;
-            }
-
-            switch (Type)
-            {
-                case 1:
-                    Skip = new[,] { { 1, 0 }, { 1, 1 } };
-                    break;
-                case 2:
-                    Skip = new[,] { { 1, 1 }, { 2, 1 } };
-                    break;
-                case 3:
-                    Skip = new[,] { { 0, 1 }, { 0, 2 } };
-                    break;
-                default:
-                    Skip = new[,] { { 0, 0 }, { 1, 0 } };
-                    break;
-            }
+            case 1:
+                Skip = new[,] { { 1, 0 }, { 1, 1 } };
+                break;
+            case 2:
+                Skip = new[,] { { 1, 1 }, { 2, 1 } };
+                break;
+            case 3:
+                Skip = new[,] { { 0, 1 }, { 0, 2 } };
+                break;
+            default:
+                Skip = new[,] { { 0, 0 }, { 1, 0 } };
+                break;
         }
     }
 }
--- Tetris/Tetraminos/TetraminoO.cs
+++ Tetris/Tetraminos/TetraminoO.cs
@@ -1,28 +1,26 @@
-﻿using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
 
-namespace Tetris
+namespace Tetris;
+
+internal class TetraminoO : Tetramino
 {
-    internal class TetraminoO : Tetramino
+    public TetraminoO(Rectangle background)
     {
-        public TetraminoO(Rectangle background)
-        {
-            Color = Color.Yellow;
-            Type = 0;
+        Color = Color.Yellow;
+        Type = 0;
 
-            Initialize(Color, background);
-        }
+        Initialize(Color, background);
+    }
 
-        public override void Rotate(Queue<Block>[] columns)
-        {
-            Initialize(Color, Blocks[0][0]);
-        }
+    public override void Rotate(Queue<Block>[] columns)
+    {
+        Initialize(Color, Blocks[0][0]);
+    }
 
-        protected override void SetProperties()
-        {
-            Columns = 2;
-            Rows = 2;
-        }
+    protected override void SetProperties()
+    {
+        Columns = 2;
+        Rows = 2;
     }
 }
